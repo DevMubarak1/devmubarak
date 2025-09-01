3020fff58b850ba7f801cac7ca49ad72a918eb92
Dev.Mubarak <rmbabatunde123@gmail.com>
2025-09-01 06:32:27 +0100

    Update 2025-09-01 06:32

---
+++ src/app/sitemap.ts
@@ -0,0 +1,37 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://devmubarak.me',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://devmubarak.me/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://devmubarak.me/about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+        url: 'https://devmubarak.me/toolbox',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.6,
+      },
+      {
+        url: 'https://devmubarak.me/blog',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.6,
+      },
+    // Add more pages if you have them
+  ];
+}
